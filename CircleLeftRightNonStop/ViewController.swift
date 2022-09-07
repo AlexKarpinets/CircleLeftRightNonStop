055f88ed4908636dfc4ac827f1c16ab0566057fa
Alexander Karpinets <alexanderkarpinets@gmail.com>
2022-09-07 22:03:35 +0300

    First commit

--- CircleLeftRightNonStop/ViewController.swift
+++ CircleLeftRightNonStop/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let circleView = UIView(frame: CGRect(x: 50, y: 50, width: 100, height: 100))
+    private let squareView = UIView(frame: CGRect(x: 100, y: 100, width: 300, height: 200))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
     }
-
-
+    
+    @IBAction func goButton(_ sender: UIButton) {
+        circleView.frame.origin.x += 100
+        UIView.animate(withDuration: 1, delay: 1, options: .repeat) {
+            self.circleView.frame.origin.x -= 200
+        }
+    }
+    
+    private func setupUI() {
+        squareView.backgroundColor = .gray
+        squareView.center = view.center
+        squareView.layer.cornerRadius = 15
+        view.addSubview(squareView)
+        
+        circleView.center = squareView.center
+        circleView.backgroundColor = .red
+        circleView.layer.cornerRadius = circleView.frame.size.width / 2
+        view.addSubview(circleView)
+    }
 }
 
